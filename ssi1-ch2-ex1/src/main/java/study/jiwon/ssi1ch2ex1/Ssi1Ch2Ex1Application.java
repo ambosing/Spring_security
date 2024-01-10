8c8a701475a1741aaec69d438c7b511307ab5443
ambosing <ambosing_@naver.com>
2024-01-10 16:02:51 +0900

    [Study] 스프링 시큐리티 HTTP basic 경험 및 자체 인증 서명으로 HTTPS 만들어서 요청 보내보기

---
+++ ssi1-ch2-ex1/src/main/java/study/jiwon/ssi1ch2ex1/Ssi1Ch2Ex1Application.java
@@ -0,0 +1,13 @@
+package study.jiwon.ssi1ch2ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssi1Ch2Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssi1Ch2Ex1Application.class, args);
+	}
+
+}
---
+++ ssi1-ch2-ex1/src/test/java/study/jiwon/ssi1ch2ex1/Ssi1Ch2Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jiwon.ssi1ch2ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssi1Ch2Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
